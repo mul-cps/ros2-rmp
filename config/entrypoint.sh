9b9f26c1458174cc3e97d39656ec7d80d881cb5c
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2024-03-28 09:58:05 +0100

    update

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -11,7 +11,7 @@ echo "Sourced ROS 2 ${ROS_DISTRO}"
 if [ -f ${UNDERLAY_WS}/install/setup.bash ]
 then
   source ${UNDERLAY_WS}/install/setup.bash
-  vcs pull ${UNDERLAY_WS}/src
+  # vcs pull ${UNDERLAY_WS}/src
   echo "Sourced CPS RMP 220 base workspace"
 fi
 
@@ -19,7 +19,7 @@ fi
 if [ -f /overlay_ws/install/setup.bash ]
 then
   source /overlay_ws/install/setup.bash
-  vcs pull /overlay_ws/src
+  # vcs pull /overlay_ws/src
   echo "Sourced CPS RMP 220 Overlay workspace"
 fi
 
@@ -32,67 +32,19 @@ then
 fi
 
 # Implement updating all repositories at launch
-if [ -f ${UNDERLAY_WS}/]
-then
-  cd ${UNDERLAY_WS}
-  vcs pull src
-  echo "Updated base workspace"
-fi
-
-if [ -f /overlay_ws/]
-then
-  cd /overlay_ws
-  vcs pull src
-  echo "Updated overlay workspace"
-fi
-
-# configure container networking to use zerotier interface as standard gateway
-# this might be for later
-# if [ -f /zerotier-one/zerotier-cli ]
+# if [ -f ${UNDERLAY_WS}/]
 # then
-#   /zerotier-one/zerotier-cli join 8056c2e21c000001
-#   echo "Joined zerotier network"
-# fi
-# # set standard gateway to zerotier interface
-# ip route del default
-# ip route add default via $(ip addr show zt* | grep -Po 'inet \K[\d.]+')
-# echo "Set zerotier interface as standard gateway"
-# this should now route the ros2 traffic through zerotier?
-
-# # Find the ZeroTier interface name dynamically
-# zerotier_interface=$(ip addr show | awk '/^.*: zt/{print $2}' | cut -d ':' -f 1)
-
-# if [ -z "$zerotier_interface" ]; then
-#   echo "ZeroTier interface not found."
-#   exit 1
+#   cd ${UNDERLAY_WS}
+#   vcs pull src
+#   echo "Updated base workspace"
 # fi
 
-# # Set the standard gateway to the ZeroTier interface
-# ip route del default
-# ip route add default via $(ip addr show "$zerotier_interface" | grep -Po 'inet \K[\d.]+')
-
-# echo "Set ZeroTier interface '$zerotier_interface' as the standard gateway."
-
-# Changing apporach to just configure the cyconedds to use the zerotier interface
-
-# For now add respective ip apps to do the stuff below:
-apt update && apt install -y iproute2
-
-# Find the ZeroTier interface name dynamically
-# zerotier_interface=$(ip addr show | awk '/^.*: zt/{print $2}' | cut -d ':' -f 1)
-
-# if [ -z "$zerotier_interface" ]; then
-#   echo "ZeroTier interface not found."
-#   exit 1
+# if [ -f /overlay_ws/]
+# then
+#   cd /overlay_ws
+#   vcs pull src
+#   echo "Updated overlay workspace"
 # fi
-
-# echo "Using ZeroTier interface: $zerotier_interface"
-
-# # Set the path to your CycloneDDS configuration file
-# config_file="/cyclonedds.xml"
-
-# # Replace the content between <NetworkInterfaceAddress> tags
-# sed -i "s|<NetworkInterfaceAddress>.*</NetworkInterfaceAddress>|<NetworkInterfaceAddress>$zerotier_interface</NetworkInterfaceAddress>|g" "$config_file"
 
 # Execute the command passed into this entrypoint
 exec "$@"
