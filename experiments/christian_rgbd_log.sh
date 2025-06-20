a50c4c2fee15fac04562f0677b1e9a9b19ec2f61
Your Name <you@example.com>
2025-06-20 09:08:16 +0200

    update experiemtn files

---
+++ experiments/christian_rgbd_log.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ros2 bag record \
+    --no-discovery \
+    --topics \
+        /camera/depth/camera_info \
+        /camera/depth/image_raw/compressed \
+        /camera/color/camera_info \
+        /camera/color/image_raw/compressed \
+        /camera/imu \
+        /tf \
+        /tf_static \
+        /odom \
+        /cmd_vel_out \
+        /scan \
+        /speed_fb \
+        /map
+    --output $1
+
+ros2 bag info $1
+
